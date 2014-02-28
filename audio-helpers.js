15433f950ebc8f8d295825884276c23e6f9ae609
Michael Garate <mpgarate@gmail.com>
2014-02-28 02:46:35 -0500

    sharps and flats

--- audio-helpers.js
+++ audio-helpers.js
@@ -1,10 +1,10 @@
-var StepsToHzNote = function(half_steps){
-	var value = Math.pow(1.059460646483, half_steps) * 440;
+var StepsToHzNote = function(halfSteps){
+	var value = Math.pow(1.059460646483, halfSteps) * 440;
 	console.log(value);
 	return value;
 }
 
-var half_steps_from_a = function(note,octave){
+var halfStepsFromA = function(note,octave){
 	// Default to octave 4
 	if (typeof octave === 'undefined'){
 		octave = 0;
@@ -41,7 +41,7 @@ var charToNoteIndex = function(s, offset){
 	// adjust for sharp or flat note
 	i += offset;
 
-	return half_steps_from_a(i);
+	return halfStepsFromA(i);
 }
 
 // offset handles sharp or flat
@@ -60,3 +60,38 @@ var StringToNoteIndex = function(s,offset){
 	}
 
 }
+
+
+  var stringToSteps = function(s,octave){
+		var offset = 0; // handle sharp or flat
+		if (typeof s === 'undefined' || s === ''){
+			s = 'A';
+		}
+		// set default or get octave from note string
+		if (octave === 'undefined'){
+			if (isNaN(s.charAt(s.length))){
+				octave = 4;	
+			}
+			else{
+				octave = s.charAt(s.length);
+				s = s.split(s.length - 1);
+			}
+		}
+
+		if(s.length > 1){
+			var char1 = s.charAt(1);
+			if(char1 === '#'){
+				offset = 1;
+			}
+			else if (char1 === 'b'){
+				offset = -1;
+			}
+		}
+
+		console.log("s: " + s + " offset: " + offset + "oct: " + octave);
+
+		var noteIndex = charToNoteIndex(s,offset);
+		var steps = halfStepsFromA(noteIndex, octave);
+
+		return steps;
+  }
--- bleep.js
+++ bleep.js
@@ -14,38 +14,13 @@ window.Bleep = (function() {
 	// duration is in 32nd notes. 32 = 1 beat. 
 	// octave is MIDI standard
 	Bleep.tone = function(note, duration, octave){
-		var offset = 0; // handle sharp or flat
-		if (typeof note === 'undefined'){
-			note = 'A';
-		}
 		if (typeof duration === 'undefined'){
 			duration = 32;
 		}
-		if (octave === 'undefined'){
-			// set default or get octave from note
-			if (!isNaN(note.charAt(note.length))){
-				octave = 4;	
-			}
-			else{
-				octave = note.charAt(note.length);
-				note = note.split(note.length -1);
-			}
+		if (typeof note === 'undefined'){
+			note = 'A';
 		}
-
-		if(note.length > 1){
-			var lastChar = note.charAt(note.length);
-			if(lastChar === '#'){
-				offset = 1;
-				note = note.split(note.length -1);
-			}
-			else if (lastChar === 'b'){
-				offset = -1;
-				note = note.split(note.length -1);
-			}
-		}
-
-		var note_index = StringToNoteIndex(note,offset);
-		var steps = half_steps_from_a(note_index, octave);
+		var steps = stringToSteps(note, octave);
 
 		
 		var oscillator = context.createOscillator();
@@ -67,6 +42,7 @@ window.Bleep = (function() {
     oscillator.stop(0);
     }, waitTime * 1.5);
   }
+
 
 	return Bleep;
 })();
