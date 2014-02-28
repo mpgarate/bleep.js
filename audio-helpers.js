f3d85bd59f4b75859156ed703018f0825e1fafcb
Michael Garate <mpgarate@gmail.com>
2014-02-28 03:18:10 -0500

    commented and cleaned up code

--- audio-helpers.js
+++ audio-helpers.js
@@ -1,9 +1,12 @@
+// Convert a signed integer distance from A in half steps
+// to a Hz value suitable for an oscillator
 var StepsToHzNote = function(halfSteps){
 	var value = Math.pow(1.059460646483, halfSteps) * 440;
-	console.log("to hz val is:" + value + " halfSteps:" + halfSteps);
+	console.log("Hz:" + value);
 	return value;
 }
 
+// Calculate how far a note is from A4 (440Hz)
 var halfStepsFromA = function(noteIndex,octave){
 	// Convert octave value
 	// MIDI octave 4 is octave 0
@@ -16,6 +19,9 @@ var halfStepsFromA = function(noteIndex,octave){
 	return noteIndex + (octave * 12);
 }
 
+// Convert an ASCII character and # or b into
+// a 0-11 note value. This note does not specify
+// an octave.
 var charToNoteIndex = function(s, offset){
 	// Get ASCII value of char
 	var i = s.charCodeAt(0);
@@ -28,18 +34,23 @@ var charToNoteIndex = function(s, offset){
 	else if(i < 65 || i > 71){
 		return null;
 	}
-	// refer to notes as in range 0-6 
+	// refer to letter notes as in range 0-6 
 	i -= 65;
 
 	// space out to # of half steps from A
 	switch(i){
-		case 0: i = 0; break; //A
-		case 1: i = 2; break; //B
-		case 2: i = 3; break; //C
-		case 3: i = 5; break; //D
-		case 4: i = 7; break; //E
-		case 5: i = 8; break; //F
-		case 6: i = 10; break; //G
+		case 0: i = 0; break;  // A
+		// skip							      A#
+		case 1: i = 2; break;  // B
+		case 2: i = 3; break;  // C
+		// skip					 		      C#
+		case 3: i = 5; break;  // D
+		// skip					 		      D#
+		case 4: i = 7; break;  // E
+		case 5: i = 8; break;  // F
+		// skip					 		      F#
+		case 6: i = 10; break; // G
+		// skip					 		      G#
 	}
 
 	// adjust for sharp or flat note
@@ -48,58 +59,39 @@ var charToNoteIndex = function(s, offset){
 	return i;
 }
 
-// offset handles sharp or flat
-var StringToNoteIndex = function(s,offset){
-	if(s === undefined){
-		return null;
+// Parse an input string like 'A' or 'B0'
+// or 'C#4' or 'Db6' into a HzNote suitable
+// for an oscillator object
+var stringToHzNote = function(s,octave){
+	var offset = 0, steps, HzNote; // handle sharp or flat
+	if (typeof s === 'undefined' || s === ''){
+		s = 'A';
 	}
-	if(typeof offset === undefined){
-		offset = 0;
-	}
-	else if(s === ""){
-		return null;
-	}
-	else if(s.length === 1){
-		return charToNoteIndex(s,offset);
+	// set default or get octave from note string
+	if (typeof octave === 'undefined'){
+		if (isNaN(s.charAt(s.length-1))){
+			octave = 4;	
+		}
+		else{
+			octave = s.charAt(s.length -1);
+			s = s.substr(0,s.length - 1);
+		}
 	}
 
+	if(s.length > 1){
+		var char1 = s.charAt(1);
+		if(char1 === '#'){
+			offset = 1;
+			s = s.substr(0,1);
+		}
+		else if (char1 === 'b'){
+			offset = -1;
+			s = s.substr(0,1);
+		}
+	}
+
+	var noteIndex = charToNoteIndex(s,offset);
+	var steps = halfStepsFromA(noteIndex, octave);
+	var HzNote = StepsToHzNote(steps);
+	return HzNote;
 }
-
-
-  var stringToHzNote = function(s,octave){
-		var offset = 0; // handle sharp or flat
-		if (typeof s === 'undefined' || s === ''){
-			s = 'A';
-		}
-		// set default or get octave from note string
-		if (typeof octave === 'undefined'){
-			if (isNaN(s.charAt(s.length-1))){
-				octave = 4;	
-			}
-			else{
-				octave = s.charAt(s.length -1);
-				s = s.substr(0,s.length - 1);
-			}
-		}
-
-		if(s.length > 1){
-			var char1 = s.charAt(1);
-			if(char1 === '#'){
-				offset = 1;
-				s = s.substr(0,1);
-			}
-			else if (char1 === 'b'){
-				offset = -1;
-				s = s.substr(0,1);
-			}
-		}
-
-		console.log("s: " + s + " offset: " + offset + " oct: " + octave);
-
-		var noteIndex = charToNoteIndex(s,offset);
-		console.log("noteIndex:" + noteIndex);
-		var steps = halfStepsFromA(noteIndex, octave);
-		console.log("steps:" + steps);
-		var HzNote = StepsToHzNote(steps);
-		return HzNote;
-  }
--- bleep.js
+++ bleep.js
@@ -10,39 +10,37 @@ window.Bleep = (function() {
 		bpm: 120
 	};
 
-	// note is a string like 'A' or 'A0'
+	// note is a string like 'A' or 'B0' or 'C#4' or 'Db6'
 	// duration is in 32nd notes. 32 = 1 beat. 
-	// octave is MIDI standard
+	// octave is in MIDI standard, 0-8
 	Bleep.tone = function(note, duration, octave){
 		if (typeof duration === 'undefined'){
 			duration = 32;
 		}
-		if (typeof note === 'undefined'){
-			note = "A";
-		}
 		var HzNote = stringToHzNote(note, octave);
-
 		
-		var oscillator = context.createOscillator();
+		var o = context.createOscillator();
 		var g = context.createGainNode();
 		currentTime = context.currentTime;
 
-		//oscillator.connect(context.destination); // Connect to speakers
-    oscillator.frequency.value = parseFloat(HzNote); // in hertz
-		oscillator.connect(g);
-    oscillator.start(0); // Start generating sound immediately
+    o.frequency.value = parseFloat(HzNote); 
+		o.connect(g);
+    o.start(0);
 		g.connect(context.destination);
 
-
 		var waitTime =  duration * (Settings.bpm / 32);
+
+		// fade note to prevent pop
     setTimeout(function(){
 			g.gain.setTargetAtTime(0, 0, 0.01);
     }, waitTime);
-    setTimeout(function(){
-    oscillator.stop(0);
-    }, waitTime * 1.5);
-  }
 
+    // kill oscillator shortly after
+    setTimeout(function(){
+    o.stop(0);
+    }, waitTime * 1.5);
+
+  }
 
 	return Bleep;
 })();
