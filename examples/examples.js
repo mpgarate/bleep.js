6103ce24afaebb83779de34526b6b54707f56e78
Michael Garate <mpgarate@gmail.com>
2014-03-09 14:54:28 -0400

    reduced default volume

--- examples/examples.js
+++ examples/examples.js
@@ -1,62 +1,18 @@
-$( document ).ready(function() { /*
-    var context = new webkitAudioContext(),
-    oscillator = context.createOscillator();
-    oscillator.connect(context.destination); // Connect to speakers
-    var PITCH = 0;
-
-  $(".start-tone").click(function(){
-    oscillator.start(0); // Start generating sound immediately
-    oscillator.frequency.value = StringToHzNote("D"); // in hertz
-  });
-
-    $(".inc-tone").click(function(){
-      PITCH +=1;
-      oscillator.frequency.value = StringToHzNote("C"); // in hertz
-    });
-    $(".dec-tone").click(function(){
-      PITCH -=1;
-      oscillator.frequency.value = StepsToHzNote(PITCH); // in hertz
-    });
-
-  $(".stop").click(function(){
-    oscillator.stop(0); // Start generating sound immediately
-  });
-
-  console.log(StepsToHzNote(1));
-  */
-
-/*
-    Bleep.tone("A4");
-    Bleep.tone("A4");
-    Bleep.setbpm(60);
-    Bleep.tone("A4");
-    Bleep.tone("A4");
-    Bleep.setbpm(120);
-    Bleep.tone("C4");
-    Bleep.tone("D4");
-    Bleep.setbpm(60);
-    Bleep.tone("F4");
-    Bleep.tone("C4");
-    Bleep.setbpm(120);
-    Bleep.tone("G4");
-    Bleep.tone("E4");
-    Bleep.setbpm(60);
-    Bleep.tone("B4");
-    Bleep.tone("C4");
-    Bleep.tone("A4");
-*/
+$( document ).ready(function() {
 
   var slider = $('#slider');
   slider.slider({
     range:"min",
-    value: 60,
+    value: 35,
     slide: function(event, ui) {   
       var value = slider.slider('value');
       value = parseFloat(value/100);
       Bleep.liveSetMasterVolume(value);
-    }
+    },
   });
 
+
+  Bleep.liveSetMasterVolume(slider.slider('value') / 100);
 
     $( "#slider" ).slider();  
 
